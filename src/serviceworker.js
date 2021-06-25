36b00754de04e0f3f9340d2dada65d8c57d6cfca
Maicon <devmaicon@gmail.com>
2021-06-25 02:56:07 -0300

    add servicework.js

---
+++ src/serviceworker.js
@@ -0,0 +1,22 @@
+export default function serviceworker() {
+    let swUrl = `/sw.js`;
+    navigator.serviceWorker.register(swUrl).then((response) => {
+        console.log("response done:-", response);
+    });
+
+    /*     if('serviceWorker' in navigator){
+        window.addEventListener('load',function(){
+            navigator.serviceWorker.register(swUrl)
+            .then(function(registration){
+                console.log("worker registration is successfull",registration.scope);
+            },function(err){
+                console.log("Failed")
+            })
+            .catch(function(err){
+                console.log(err)
+            })
+        })
+    }else{
+        console.log("Service worker is not supported")
+    } */
+}
